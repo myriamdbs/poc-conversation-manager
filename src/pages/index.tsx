98ba25b37be2fd4131f954fd66345551a3ec6baa
Myriam <myriam.dubois@swile.co>
2022-06-05 16:13:14 +0200

    refactor: move conversation list logic into a dedicated component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,11 @@
 import { FC, useEffect, useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import Logo from '../assets/lbc-logo.webp'
 import styles from '../styles/Home.module.css'
 import { loggedUserId } from './_app'
 import { Conversation } from '../types/conversation'
+import ConversationsList from '../components/ConversationsList'
 
 const Home: FC = () => {
-  const [conversations, setConversations] = useState<Conversation[]>([])
-
-  useEffect(() => {
-    const getConversationsByUserId = async (userId: string) => {
-      const response = await fetch(
-        `http://localhost:3005/conversations/${userId}`
-      )
-      const conversationsResults = response.json()
-      console.log(conversationsResults)
-      return conversationsResults
-    }
-    getConversationsByUserId(loggedUserId.toString()).then((conversations) => {
-      return setConversations(conversations)
-    })
-  }, [])
-
   return (
     <div className={styles.container}>
       <Head>
@@ -31,9 +14,7 @@ const Home: FC = () => {
       </Head>
 
       <main className={styles.main}>
-        {conversations.map((conversation) => {
-          return <div key={conversation.id}>{conversation.id}</div>
-        })}
+        {loggedUserId && <ConversationsList loggedUserId={loggedUserId} />}
       </main>
     </div>
   )
