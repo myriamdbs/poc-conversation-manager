235ed3e4110ccaea1eee12746580b6672e5bfb26
Myriam <myriam.dubois@swile.co>
2022-06-06 13:25:02 +0200

    refactor: move post message fn to service

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,3 +5,26 @@ export const getMessagesByConversationsId = async (conversationId: string) => {
   const messagesResults = response.json()
   return messagesResults
 }
+
+export const postNewMessage = async (
+  conversationId: string,
+  messageContent: string,
+  loggedUserId: number
+) => {
+  const date = new Date().toDateString()
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      body: messageContent,
+      timestamp: Date.parse(date) / 1000,
+      authorId: loggedUserId,
+      conversationId: conversationId,
+    }),
+  }
+
+  await fetch(
+    `http://localhost:3005/messages/${conversationId}`,
+    requestOptions
+  )
+}
